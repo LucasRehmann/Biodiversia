7ee7f7b9a98fe951c61a0b55c40e78928e6e3081
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-08-29 09:40:33 -0300

    continued
    Map added

--- MapaSitioWeb/index.js
+++ MapaSitioWeb/index.js
@@ -1,8 +1,9 @@
-const boton = document.querySelector(".see-more");
-boton.onClick = ()=>{
-    
-}
+mapboxgl.accessToken = 'pk.eyJ1IjoibWp2YWxlbnp1ZWxhIiwiYSI6ImNrb2Fmdm9zZDBpM28ybnFtYTQ2Z2MwMnYifQ.ZY3jTw0-6tjUSOOJXJHsdw'
 
-//    let clickHandler = (e) => {
-  //      console.log(e)
-    //}
+var map = new mapboxgl.Map({
+	container: "map",
+	style: 'mapbox://styles/mapbox/satellite-streets-v11',
+	//mapbox://styles/mapbox/satellite-v9
+	center: [-60,-40],
+	zoom: 3.5
+});
